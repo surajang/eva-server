266262991d52bb213928563096ccac2906d3392a
손민식[surajang][Platform Design] <memswap@gmail.com>
2024-11-14 19:23:33 +0900

    remove hostname

--- main.js
+++ main.js
@@ -1,12 +1,11 @@
 const http = require('http');
 
-const hostname = 'localhost';
 const port = 8021;
 
 http.createServer((req, res) => {
     res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
 	res.write('<h1>Hello, World!</h1>');
 	res.end();
-}).listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
+}).listen(port, () => {
+    console.log(`Server running at localhost:${port}/`);
 }); 
