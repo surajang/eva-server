5e0bbbc895a26c740baed171e70fee0e87e01b5e
손민식[surajang][Platform Design] <memswap@gmail.com>
2024-11-14 19:17:22 +0900

    add server running status stdout

--- main.js
+++ main.js
@@ -1,7 +1,12 @@
 const http = require('http');
 
+const hostname = 'localhost';
+const port = 8021;
+
 http.createServer((req, res) => {
     res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
 	res.write('<h1>Hello, World!</h1>');
 	res.end();
-}).listen(8021);
+}).listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+}); 
